56dcd1e6d39bb64666a4129934af6400774e88c1
haftamudesta <desta451616@hotmail.com>
2026-07-09 12:19:53 -0700

    Add users controller

--- apps/api/src/api.module.ts
+++ apps/api/src/api.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { ApiController } from './api.controller';
 import { ApiService } from './api.service';
 import { UsersService } from './modules/users/users.service';
+import { UsersController } from './modules/users/users.controller';
+import { UsersModule } from './modules/users/users.module';
 
 @Module({
-  imports: [],
-  controllers: [ApiController],
+  imports: [UsersModule],
+  controllers: [ApiController, UsersController],
   providers: [ApiService, UsersService],
 })
 export class ApiModule {}
